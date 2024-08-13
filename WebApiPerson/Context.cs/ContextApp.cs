a01489d3367563de8adbf0f45d7156c1da989c1c
Nicolle Rosa <niicostars04@gmailcom>
2024-08-12 20:48:33 -0400

    repo de apiperson

---
+++ WebApiPerson/Context.cs/ContextApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.Context.cs
+{
+    public class ContextApp: DbContext
+    {
+        public ContextApp(DbContextOptions<ContextApp> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
